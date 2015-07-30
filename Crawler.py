311af210bd1403d0bfad4804c4595430cb2e6cb2
yuvalar879 <yuvalar879@users.noreply.github.com>
2015-07-30 16:26:09 +0300

    Release

---
+++ Crawler.py
@@ -0,0 +1,18 @@
+__author__ = 'YUVAL'
+
+import webbrowser, requests, bs4, pprint
+
+url = 'http://www.nexusmods.com/skyrim/mods/top/?'
+
+res = requests.get(url)
+res.raise_for_status()
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+File_Names = soup.find_all('a', attrs={"class": "file"})
+
+for file in File_Names:
+    s = str(file.text)
+    s = s.strip().replace('for', '').replace('Skyrim', '')
+    print(s)
+
+# Another possibility
+x = soup.select(".file")
